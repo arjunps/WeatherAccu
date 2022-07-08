2909a25861f1b9299d4fcc32f38366e990ca04f6
Arjun P S <arjunps1994@gmail.com>
2022-07-08 12:55:25 +0530

    refactoring on city card view

--- WeatherAccu/Main/Home/View/WeatherHomeView.swift
+++ WeatherAccu/Main/Home/View/WeatherHomeView.swift
@@ -19,13 +19,14 @@ struct WeatherHomeView: View {
                     CelciusOrFarenheitView(viewModel: viewModel)
                     // Show major cities weather data
                     if let cuurentCityWeatherData = viewModel.currentLocationWeatherData {
-                        CurrentLocationWeatherView(name: cuurentCityWeatherData.name,
-                                                   temp: cuurentCityWeatherData.weatherData?.main.temp,
-                                                   tempLow: cuurentCityWeatherData.weatherData?.main.tempMin,
-                                                   tempHigh: cuurentCityWeatherData.weatherData?.main.tempMax,
-                                                   climate: cuurentCityWeatherData.weatherData?.weather.first?.description,
-                                                   icon: cuurentCityWeatherData.weatherData?.weather.first?.icon,
-                                                   showFarenheitValue: viewModel.showFarenheitValue)
+                        CityWeatherCardView(name: cuurentCityWeatherData.name,
+                                            temp: cuurentCityWeatherData.weatherData?.main.temp,
+                                            tempLow: cuurentCityWeatherData.weatherData?.main.tempMin,
+                                            tempHigh: cuurentCityWeatherData.weatherData?.main.tempMax,
+                                            climate: cuurentCityWeatherData.weatherData?.weather.first?.description,
+                                            icon: cuurentCityWeatherData.weatherData?.weather.first?.icon,
+                                            showFarenheitValue: viewModel.showFarenheitValue,
+                                            isCurrentLocation: true)
                     }
                     
                     // Show if current location weather data is available.
@@ -36,7 +37,8 @@ struct WeatherHomeView: View {
                                             tempHigh: city.weatherData?.main.tempMax,
                                             climate: city.weatherData?.weather.first?.description,
                                             icon: city.weatherData?.weather.first?.icon,
-                                            showFarenheitValue: viewModel.showFarenheitValue)
+                                            showFarenheitValue: viewModel.showFarenheitValue,
+                                            isCurrentLocation: false)
                     }
                 }
                 .padding()
@@ -47,6 +49,7 @@ struct WeatherHomeView: View {
     }
 }
 
+
 fileprivate struct CityWeatherCardView: View {
     let name: String
     let temp: Double?
@@ -55,28 +58,37 @@ fileprivate struct CityWeatherCardView: View {
     let climate: String?
     let icon: String?
     let showFarenheitValue: Bool
+    let isCurrentLocation: Bool
     
     var body: some View {
         VStack(alignment: .leading, spacing: 12) {
+            if isCurrentLocation {
+                Text("Current Location")
+                    .font(.body)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .fixedSize(horizontal: false, vertical: true)
+            }
+            
             HStack {
                 VStack(alignment: .leading) {
                     Text(name)
                         .font(.title3)
                         .fontWeight(.medium)
-                        .foregroundColor(Color.black)
+                        .foregroundColor(isCurrentLocation ? .white: .black)
                         .fixedSize(horizontal: false, vertical: true)
                     if let tempConverted = temp?.getKelvinToCelciusOrFarenheit(toCelcius: !showFarenheitValue) {
                         Text("\(tempConverted)")
                             .font(.largeTitle)
                             .fontWeight(.light)
-                            .foregroundColor(Color.black)
+                            .foregroundColor(isCurrentLocation ? .white: .black)
                             .fixedSize(horizontal: false, vertical: true)
                     }
                     if let tempConvertedLow = tempLow?.getKelvinToCelciusOrFarenheit(toCelcius: !showFarenheitValue), let tempConvertedHigh = tempHigh?.getKelvinToCelciusOrFarenheit(toCelcius: !showFarenheitValue) {
                         Text("H:\(tempConvertedHigh) / L:\(tempConvertedLow)")
                             .font(.caption)
                             .fontWeight(.regular)
-                            .foregroundColor(Color.black)
+                            .foregroundColor(isCurrentLocation ? .white: .black)
                             .fixedSize(horizontal: false, vertical: true)
                     }
                 }
@@ -93,7 +105,7 @@ fileprivate struct CityWeatherCardView: View {
                         Text(climateDesc.uppercased())
                             .font(.caption)
                             .fontWeight(.regular)
-                            .foregroundColor(Color.black)
+                            .foregroundColor(isCurrentLocation ? .white: .black)
                             .fixedSize(horizontal: false, vertical: true)
                     }
                 }
@@ -102,7 +114,7 @@ fileprivate struct CityWeatherCardView: View {
         .padding()
         .background(
             Rectangle()
-                .fill(Color.white)
+                .fill(isCurrentLocation ? Color(hex: "255F85") : Color.white)
                 .cornerRadius(12)
                 .shadow(
                     color: Color.gray.opacity(0.3),
@@ -113,77 +125,7 @@ fileprivate struct CityWeatherCardView: View {
     }
 }
 
-fileprivate struct CurrentLocationWeatherView: View {
-    let name: String
-    let temp: Double?
-    let tempLow: Double?
-    let tempHigh: Double?
-    let climate: String?
-    let icon: String?
-    let showFarenheitValue: Bool
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text("Current Location")
-                .font(.body)
-                .fontWeight(.bold)
-                .foregroundColor(Color.white)
-                .fixedSize(horizontal: false, vertical: true)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text(name)
-                        .font(.title3)
-                        .fontWeight(.medium)
-                        .foregroundColor(Color.white)
-                        .fixedSize(horizontal: false, vertical: true)
-                    if let tempConverted = temp?.getKelvinToCelciusOrFarenheit(toCelcius: !showFarenheitValue) {
-                        Text("\(tempConverted)")
-                            .font(.largeTitle)
-                            .fontWeight(.light)
-                            .foregroundColor(Color.white)
-                            .fixedSize(horizontal: false, vertical: true)
-                    }
-                    if let tempConvertedLow = tempLow?.getKelvinToCelciusOrFarenheit(toCelcius: !showFarenheitValue), let tempConvertedHigh = tempHigh?.getKelvinToCelciusOrFarenheit(toCelcius: !showFarenheitValue) {
-                        Text("H:\(tempConvertedHigh) / L:\(tempConvertedLow)")
-                            .font(.caption)
-                            .fontWeight(.regular)
-                            .foregroundColor(Color.white)
-                            .fixedSize(horizontal: false, vertical: true)
-                    }
-                }
-                
-                Spacer()
-                VStack(alignment: .trailing) {
-                    if let icon = icon, let url =  URL(string: "http://openweathermap.org/img/wn/\(icon)@2x.png") {
-                        AsyncImage(url: url) {
-                            CustomLoadingView()
-                        }
-                        .frame(width: 64, height: 64, alignment: .center)
-                    }
-                    if let climateDesc = climate {
-                        Text(climateDesc.uppercased())
-                            .font(.caption)
-                            .fontWeight(.regular)
-                            .foregroundColor(Color.white)
-                            .fixedSize(horizontal: false, vertical: true)
-                    }
-                }
-            }
-        }
-        .padding()
-        .background(
-            Rectangle()
-                .fill(Color(hex: "255F85"))
-                .cornerRadius(12)
-                .shadow(
-                    color: Color.gray.opacity(0.3),
-                    radius: 3,
-                    x: 0,
-                    y: 0
-                ))
-    }
-}
-
+// Toggle view to support Farenheit conversion
 fileprivate struct CelciusOrFarenheitView: View {
     @ObservedObject var viewModel: WeatherHomeViewModel
     
