12156f458eb1c6682855bd785d519e364f618cb5
Arjun P S <arjunps1994@gmail.com>
2022-07-08 11:50:12 +0530

    added comments and remove unused code.

--- WeatherAccu/Managers/WeatherManager.swift
+++ WeatherAccu/Managers/WeatherManager.swift
@@ -10,22 +10,9 @@ import Foundation
 
 class WeatherManager {
     // HTTP request to get the current weather depending on the coordinates we got from LocationManager
-    static func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> WeatherResponseBody? {
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=81c597afe02bfd7efe78456f6ef75eae") else { return nil }
-
-
-        let urlRequest = URLRequest(url: url)
-        
-        let (data, response) = try await URLSession.shared.data(for: urlRequest)
-        
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching data") }
-        
-        let decodedData = try JSONDecoder().decode(WeatherResponseBody.self, from: data)
-        
-        return decodedData
-    }
-    
     static func getWeatherData(latitude: CLLocationDegrees, longitude: CLLocationDegrees, completion: @escaping (WeatherResponseBody?, Error?) -> Void) {
+        
+        // API key should be added from a config.
         let urlRequest = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=81c597afe02bfd7efe78456f6ef75eae")!
 
         var request = URLRequest(url: urlRequest)
